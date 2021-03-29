8caaf70de477a96e55675cc2d919252e2931dc04
Mostafa Shahat <mostafa.shahat2014@gmail.com>
2021-03-29 21:14:34 +0200

    Git Test

---
+++ new.js
@@ -0,0 +1,12 @@
+/*global console, alert*/
+
+document.getElementById("test").innerHTML="WELCOM To JavaScript";
+
+var ticketPrice = 900;
+if(ticketPrice < 2000 ) {
+    console.log("Oooh Yeaaah");
+} else if (ticketPrice == 900) {
+    console.log("godd ticketPrice");
+} else if (ticketPrice > 2000) {
+    console,log("OOOh Noooo");
+}
